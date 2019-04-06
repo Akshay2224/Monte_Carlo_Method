fb13a64e76ea4d137f875441e47122c9c8784e43
Akshay2224 <31189471+Akshay2224@users.noreply.github.com>
2019-04-06 12:16:02 +0530

    Update project.c

--- project.c
+++ project.c
@@ -7,35 +7,46 @@
 void *fun1();
 void *fun2();
 int count=0;
+int s=0;
 pthread_mutex_t l;
 int n;
 int main()
 {
- pthread_t t1,t2;
+ pthread_t t1,t2,t3,t4;
  pthread_mutex_init(&l,NULL);
+ printf("-------------------------------------------------------------WELCOME-------------------------------------------------------------------------------------\n\n\n\n");
+ printf("Enter the Iterations You want to check your values check:");
+ scanf("%d",&n);
  pthread_create(&t1,NULL,&fun1,NULL);
+ pthread_create(&t4,NULL,&fun1,NULL);
+ pthread_create(&t3,NULL,&fun1,NULL);
  pthread_create(&t2,NULL,&fun2,NULL);
  pthread_join(t1,NULL);
+ pthread_join(t4,NULL);
+ pthread_join(t3,NULL);
  pthread_join(t2,NULL);
- printf("The Number of the values are %d:",count);
 }
 void *fun1()
 {
-  int i,k;
+  int i,k,sq=0;
   double x,y,z;
   pthread_mutex_lock(&l);
-  printf("Enter the values you want to enter :");
-  scanf("%d",&n);
-  srand(SEED);
+   count=0;
+   s=0;
+  srand(time(NULL));
   for(i=0;i<n;i++)
   {
-   x=(double)rand()/RAND_MAX;
-   y=(double)rand()/RAND_MAX;
+   x=(double)(rand() % (n+1))/n;
+   y=(double)(rand()%(n+1))/n;
    z=x*x+y*y;
+   sleep(1);
+   printf("The Random Number Generated are ::\n\n");
+   printf("%g   %g  \n\n\n\n",x,y);
    if(z<=1)
    {
     count++;
    }
+   s++;
   }
  pthread_mutex_unlock(&l);
 }
@@ -43,25 +54,8 @@ void *fun2()
 {
  pthread_mutex_lock(&l);
   double pi;
-  pi=(double)count/n*4;
-  printf("# of trials= %d , estimate of pi is %g \n",n,pi);
+  pi=(double)(4*count)/s;
+  printf("We got an estimated value of pi as %g \n",pi);
+  printf("-------------------------------------------");
  pthread_mutex_unlock(&l);
 }
-/*   double z;
-   double pi;
-
-   printf("Enter the number of iterations used to estimate pi: ");
-   scanf("%d",&niter);
-
-   /* initialize random numbers 
-   srand(SEED);
-   count=0;
-   for ( i=0; i<niter; i++) {
-      x = (double)rand()/RAND_MAX;
-      y = (double)rand()/RAND_MAX;
-      z = x*x+y*y;
-      if (z<=1){ count++;}
-      }
-    printf("%d",count);
-   pi=(double)count/niter*4;
-   printf("# of trials= %d , estimate of pi is %g \n",niter,pi);*/
